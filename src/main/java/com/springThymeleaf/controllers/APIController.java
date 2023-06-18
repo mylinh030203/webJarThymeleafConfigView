5e14dd3fc76b7c7c7feddf7cffe81788a21082c4
Phan Mỹ Linh <havana03022003@gmail.com>
2023-06-18 22:01:11 +0700

    api put and delete

--- src/main/java/com/springThymeleaf/controllers/APIController.java
+++ src/main/java/com/springThymeleaf/controllers/APIController.java
@@ -1,5 +1,6 @@
 package com.springThymeleaf.controllers;
 
+import java.security.PublicKey;
 import java.util.Enumeration;
 import java.util.List;
 
@@ -10,14 +11,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.springThymeleaf.services.SinhvienService;
+import com.springThymeleaf.vos.SinhvienUpdateVO;
 import com.springThymeleaf.vos.SinhvienVO;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 
 @Validated
@@ -38,5 +45,16 @@ public class APIController {
 		int sinhVienId = sinhvienService.save(sinhvienVO);
         return ResponseEntity.ok().body(sinhvienService.getById(sinhVienId));
     }
-	
+	@RequestMapping(method = RequestMethod.PUT, path = "sinhvien/edit/{id}", produces = "application/json; charset=utf-8")
+	@ResponseBody
+	public ResponseEntity<Object> update (@RequestBody SinhvienUpdateVO VOupdate, @Valid @NotNull @PathVariable("id") int id){
+		this.sinhvienService.update(id, VOupdate);
+		return ResponseEntity.ok().body(sinhvienService.getById(id));
+	}
+	@RequestMapping(method = RequestMethod.DELETE, path = "sinhvien/delete/{id}", produces = "application/json; charset=utf-8")
+	@ResponseBody
+	public ResponseEntity<Object> delete(@Valid @NotNull @PathVariable("id") int id){
+		sinhvienService.delete(id);
+		return ResponseEntity.ok().body(id);
+	}
 }
--- src/main/java/com/springThymeleaf/vos/SinhvienVO.java
+++ src/main/java/com/springThymeleaf/vos/SinhvienVO.java
@@ -24,29 +24,29 @@ public class SinhvienVO implements Serializable {
 		return "SinhvienVO [name=" + name + ", lop=" + lop + ", tuoi=" + tuoi + "]";
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getLop() {
-		return lop;
-	}
-
-	public void setLop(String lop) {
-		this.lop = lop;
-	}
-
-	public Integer getTuoi() {
-		return tuoi;
-	}
-
-	public void setTuoi(Integer tuoi) {
-		this.tuoi = tuoi;
-	}
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getLop() {
+//		return lop;
+//	}
+//
+//	public void setLop(String lop) {
+//		this.lop = lop;
+//	}
+//
+//	public Integer getTuoi() {
+//		return tuoi;
+//	}
+//
+//	public void setTuoi(Integer tuoi) {
+//		this.tuoi = tuoi;
+//	}
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
